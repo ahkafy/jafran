a3dd5d064e0fe6f0f54bee5762123ee04b147d8f
Abdullah hel kafy <ahkafy@gmail.com>
2025-09-08 07:32:39 +0600

    fix(wallet): use .container so Recent Transactions align

--- resources/views/wallet/index.blade.php
+++ resources/views/wallet/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container">
     <!-- Page Header -->
     <div class="row mb-4">
         <div class="col-12">
